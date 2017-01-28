db5dea5e589a216d813b0de79456829b702c6214
Kristian Trandem <kkrorkk@gmail.com>
2017-01-28 20:20:39 +0100

    everythin

---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.text.*;
+
+public class Main extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, (java.lang.String[])null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            StackPane page = (StackPane) FXMLLoader.load(Main.class.getResource("test.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Endomondo");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
